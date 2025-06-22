f83ba5ad017f0eadfc5692fbcbddc1da3cc62a96
Driptanil Roy <153745351+driptanilroy@users.noreply.github.com>
2025-06-22 08:27:52 +0000

    add numbers -- user input

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,20 @@
-console.log("roy")
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+rl.question('Enter the first number: ', (first) => {
+  rl.question('Enter the second number: ', (second) => {
+    const num1 = Number(first);
+    const num2 = Number(second);
+    const result = addNumbers(num1, num2);
+    console.log("The sum is:", result);
+    rl.close();
+  });
+});
